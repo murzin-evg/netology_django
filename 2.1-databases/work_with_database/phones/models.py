32b30d6a775d54f35a01d893de108e057e30a7cb
murzin <murzin.e.s@mail.ru>
2023-06-04 13:20:50 +0300

    dj-hw-db1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,15 @@
+from django.utils.text import slugify
 from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=True)
+    slug = models.SlugField(allow_unicode=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
